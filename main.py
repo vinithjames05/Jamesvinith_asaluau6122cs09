d161fc6e63157b3cc9abac2de0cbf7c8eb2ffda6
vinithjames05 <144310606+vinithjames05@users.noreply.github.com>
2023-09-07 11:07:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+def factorial(x):
+    """This is a recursive function
+    to find the factorial of an integer"""
+
+    if x == 1:
+        return 1
+    else:
+      
+        return (x * factorial(x-1))
+
+num = 7
+result = factorial(num)
+print("The factorial of", num, "is", result)
